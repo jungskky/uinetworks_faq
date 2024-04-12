825ad6400ff80bf1075edefa2170c8d729bfc5ed
jungskky <38771771+jungskky@users.noreply.github.com>
2024-04-12 17:49:17 +0900

    Add files via upload

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://st.depositphotos.com/1036174/2667/i/450/depositphotos_26677269-stock-photo-small-person-and-word-answer.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://dragonflytraining.files.wordpress.com/2013/10/man-with-question-01.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
