7a4dbd2eed5057839da6fa9d28019a576b36552d
jungskky <38771771+jungskky@users.noreply.github.com>
2024-04-12 18:08:13 +0900

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://st.depositphotos.com/1036174/2667/i/450/depositphotos_26677269-stock-photo-small-person-and-word-answer.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://dragonflytraining.files.wordpress.com/2013/10/man-with-question-01.png">
+        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
